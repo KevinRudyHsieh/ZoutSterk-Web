0245bdcc1662f5aaa92d86a17a6ff185730b9442
KevinRudyHsieh <117444524+KevinRudyHsieh@users.noreply.github.com>
2026-06-06 19:06:40 +0200

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>EcoCore Systems | Portaal</title>
+</head>
+<body>
+    <h1>Welkom bij het EcoCore Systems Portaal</h1>
+    <p>Dit systeem beheert de circulaire hardware-cyclus.</p>
+</body>
+</html>
